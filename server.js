57cab80fad5917fd01859ee1c47a65fc2e2be73d
Monica Rangel <monicarangel95@github.com>
2020-09-12 23:51:42 -0700

    create server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const htmlRouter = require("./routes/htmlRoutes");
+const apiRouter = require("./routes/apiRoutes");
+const db = mongoose.connection;
+const PORT = process.env.PORT || 3000;
+const Exercise = require("./models/exerciseModel.js");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//create initial root route 
+app.use(express.static("public"));
+
+//might have to change the db name depending on what i call it
+mongoose.connect('mongodb+srv://Monica-95:12345@cluster0.ii1sf.mongodb.net/test', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("connection success"));
+
+app.use(htmlRouter);
+app.use(apiRouter);
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
